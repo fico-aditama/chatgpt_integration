4ecda5114941809886410ba3dfbe8591825ca56a
Fiko Aditama <fiko@simise.id>
2024-05-19 00:51:15 +0700

    20240519 - Module Custom Update

---
+++ gitpush.sh
@@ -0,0 +1,6 @@
+git init .
+git add .
+git commit -m "20240519 - Module Custom Update"
+git branch -M 14.0
+git remote add origin https://github.com/fico-aditama/odoo_custom.git
+git push -u origin 14.0
